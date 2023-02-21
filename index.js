80891d64d89d2abee5a7c21ebe5fb6f6fd64f8f1
DevJhun <113248443+codrJhun@users.noreply.github.com>
2023-02-20 18:47:43 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,41 @@
     navBar.classList.toggle("active1")
     navBar.classList.toggle("active")
   }
+
+
+var navItems = document.querySelectorAll(".nav-bar ul li a");
+
+navItems.forEach(function(item) {
+  item.addEventListener("click", function() {
+    navItems.forEach(function(item) {
+      item.classList.remove("active");
+    });
+    this.classList.add("active");
+  });
+});
+
+var home = document.querySelector(".nav-bar ul li a")
+
+window.addEventListener("load", function(){
+
+  home.classList.add("active")
+})
+
+
+
+
+const aboutMe = document.querySelector('#about-me');
+
+function animateAboutMe() {
+  aboutMe.classList.add("animate");
+  console.log('animateDiv() function called');
+}
+
+window.addEventListener('scroll', function() {
+  const rect = aboutMe.getBoundingClientRect();
+  const windowHeight = window.innerHeight;
+
+  if (rect.top <= windowHeight && rect.bottom >= 0) {
+    animateAboutMe();
+  }
+});
